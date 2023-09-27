715605e71669beef55f14f9c20ec92cbd1842e4d
Benjamin Thomas <Benjamingathomas@gmail.com>
2023-09-27 09:42:55 -0400

    added useEffect to call the races and added ability modifier to each stat

--- characterstats/src/App.js
+++ characterstats/src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import './App.css'; 
+import pointbuy from './picture/pointbuy.png';
 
 function App() {
   const [ abilityScore, setAbilityScore] = useState(27);
+  const [raceSelected, setRaceSelected] = useState('');
 
   const [strScore, setStrScore] = useState(8);
   const [dexScore, setDexScore] = useState(8);
@@ -28,7 +30,9 @@ function App() {
   const [totalWis, setTotalWis] = useState(8);
   const [totalCha, setTotalCha] = useState(8);
   const [totalCus, setTotalCus] = useState(8);
-  
+
+
+//this function is used for the math to update the scores of each ability  
 function updateScores(plusOrMinus, attributeType) {
   if (plusOrMinus === 'plus') {
     setAbilityScore(abilityScore - 1);
@@ -40,7 +44,7 @@ function updateScores(plusOrMinus, attributeType) {
     case 'strength':
       if (plusOrMinus === 'plus') {
         setStrScore(strScore + 1);
-        setTotalStr(totalStr + 1)
+        setTotalStr(totalStr + racialStr + 1)
       } else {
         setStrScore(strScore - 1);
         setTotalStr(totalStr - 1)
@@ -111,61 +115,117 @@ function updateScores(plusOrMinus, attributeType) {
   }
 }
 
-//what if we use the racechoice as the function and then call each function racial in the racechoice function?
-function raceChoice(elfRacial, human, halfOrc,halfElf, gnome, tiefling, dwarf, halfling, dragonborn,) {
-  if (raceChoice === elfRacial)
-    {
-      racialDex = 2;
-    } else if (raceChoice === human)
-    {
-      racialStr = 1;
-      racialDex = 1;
-      racialCon = 1;
-      racialInt = 1;
-      racialWis = 1;
-      racialCha = 1;
-      racialCus = 1;
-    } else if (raceChoice === halfOrc) {
-      racialStr = 2;
-      racialCon = 1;
-    } else if (raceChoice === halfElf) {
-      racialCha = 1;
-      racialDex = 1;
-      racialInt = 1;
-    } else if (raceChoice === gnome) {
-      racialInt = 2;
-    } else if (raceChoice === tiefling) {
-      racialInt = 1;
-      racialCha = 2;
-    } else if (raceChoice === dwarf) {
-      racialCon = 2;
-    } else if (raceChoice === halfling) {
-      racialDex = 2;
-    } else if (raceChoice === dragonborn) {
-      racialStr = 2;
-      racialCha = 1;
+  useEffect(() => {
+    if (raceSelected === 'Human') {
+      setRacialStr(1);
+      setTotalStr(racialStr + strScore);
 
-      return;
+      setRacialDex(1);
+      setTotalDex(racialDex + dexScore);
+
+      setRacialCon(1);
+      setTotalCon(racialCon + conScore);
+
+      setRacialInt(1);
+      setTotalInt(racialInt + intScore);
+
+      setRacialWis(1);
+      setTotalWis(racialWis + wisScore);
+
+      setRacialCha(1);
+      setTotalDex(racialCha + chaScore);
+
+      setRacialCus(1);
+      setTotalCus(racialCus + cusScore);
     }
-  }
+  }, [raceSelected])
 
+
+
+//Formula to get the ability modifier for each stat
+
+function strMod() {
+  let modifier = Math.floor((totalStr - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function dexMod() {
+  let modifier = Math.floor((totalDex - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function conMod() {
+  let modifier = Math.floor((totalCon - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function intMod() {
+  let modifier = Math.floor((totalInt - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function wisMod() {
+  let modifier = Math.floor((totalWis - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function chaMod() {
+  let modifier = Math.floor((totalCha - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
+
+function cusMod() {
+  let modifier = Math.floor((totalCus - 10) / 2)
+  if (modifier > 0) {
+    return <span>+ {modifier}</span>
+  } else {
+    return <span> {modifier}</span>
+  }
+}
 
   return (
     <div >
       <div>
         <label>
           <h1>Choose your Race</h1>
-          <select onChange={raceChoice}>
+          <select 
+            value={raceSelected}
+            onChange={e => setRaceSelected(e.target.value)}
+          >
             <option>Select</option>
-            <option onSelect={elfRacial}>Elf</option>
-            <option onSelect={humanRacial}>Human</option>
-            <option onSelect={halfOrcRacial}>Half-Orc</option>
+            <option value="Elf">Elf</option>
+            <option value="Human">Human</option>
+            {/* <option onSelect={halfOrcRacial}>Half-Orc</option>
             <option onSelect={halfElfRacial}>Half-Elf</option>
             <option onSelect={gnomeRacial}>Gnome</option>
             <option onSelect={tieflingRacial}>Tiefling</option>
             <option onSelect={dwarfRacial}>Dwarf</option>
             <option onSelect={halflingRacial}>Halfing</option>
-            <option onSelect={dragonbornRacial}>Dragonborn</option>
+            <option onSelect={dragonbornRacial}>Dragonborn</option> */}
           </select>
         </label>
       </div>
@@ -175,70 +235,62 @@ function raceChoice(elfRacial, human, halfOrc,halfElf, gnome, tiefling, dwarf, h
 
       <p>You start out with 27 ability points to spend. Your base stats start at 8 and can increase or decrease from there. The table below lets you know how many points you spend per increment or decrement. Currently it is set to RAW rules where you can't get above a 15 unless it is through racial traits.</p>
       <h1>{abilityScore}</h1>
+      <img src={pointbuy} alt='cost'/>
     </div>
     <hr></hr>
     <div>
       <button onClick={() => updateScores('plus', 'strength')}>+</button>
       <button onClick={() => updateScores('minus', 'strength')}>-</button>
-      <h4>Strength {strScore} + {racialStr} = {totalStr}</h4>
+      <h4>Strength {strScore} + Racial: {racialStr} = Total: {totalStr}</h4>
+      <h3>Modifier: {strMod()} </h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'dexterity')}>+</button>
       <button onClick={() => updateScores('minus', 'dexterity')}>-</button>
-      <h4>Dexterity {dexScore} + {racialDex} = {totalDex}</h4>
+      <h4>Dexterity {dexScore} + Racial: {racialDex} = Total: {totalDex}</h4>
+      <h3>Modifier: {dexMod()}</h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'constitution')}>+</button>
       <button onClick={() => updateScores('minus', 'constitution')}>-</button>
-      <h4>Constitution {conScore} + {racialCon} = {totalCon}</h4>
+      <h4>Constitution {conScore} + Racial: {racialCon} = Total: {totalCon}</h4>
+      <h3>Modifier: {conMod()}</h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'intelligence')}>+</button>
       <button onClick={() => updateScores('minus', 'intelligence')}>-</button>
-      <h4>Intelligence {intScore} + {racialInt} = {totalInt}</h4>
+      <h4>Intelligence {intScore} + Racial: {racialInt} = Total: {totalInt}</h4>
+      <h3>Modifier: {intMod()}</h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'wisdom')}>+</button>
       <button onClick={() => updateScores('minus', 'wisdom')}>-</button>
-      <h4>Wisdom {wisScore} + {racialWis} = {totalWis}</h4>
+      <h4>Wisdom {wisScore} + Racial: {racialWis} = Total: {totalWis}</h4>
+      <h3>Modifier: {wisMod()}</h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'charisma')}>+</button>
       <button onClick={() => updateScores('minus', 'charisma')}>-</button>
-      <h4>Charisma {chaScore} + {racialCha} = {totalCha}</h4>
+      <h4>Charisma {chaScore} + Racial: {racialCha} = Total: {totalCha}</h4>
+      <h3>Modifier: {chaMod()}</h3>
     </div>
     <hr></hr>
     <div>
     <button onClick={() => updateScores('plus', 'custom')}>+</button>
       <button onClick={() => updateScores('minus', 'custom')}>-</button>
-      <h4>Custom {cusScore} + {racialCus} = {totalCus}</h4>
+      <h4>Custom {cusScore} + Racial: {racialCus} = Total {totalCus}</h4>
+      <h3>Modifier: {cusMod()}</h3>
     </div>
 
     </div>
   );
 }
 
-function elfRacial(){};
 
-function humanRacial(){};
-
-function dragonbornRacial(){};
-
-function halfElfRacial(){};
-
-function halflingRacial(){};
-
-function halfOrcRacial(){};
-
-function dwarfRacial(){};
-
-function tieflingRacial(){};
-
-function gnomeRacial(){};
 
 export default App;
